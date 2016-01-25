e162c0ca25d9c7cefab6d89c851727c292ef1205
Eric Tipton <eric.tipton@rigor.com>
2016-01-25 02:37:15 -0800

    add code to utilize (optional) ETHON_CURL_LIBDIR ENV variable
    see comment in diff for more details

--- lib/ethon/curls/settings.rb
+++ lib/ethon/curls/settings.rb
@@ -3,6 +3,13 @@ module Ethon
     callback :callback, [:pointer, :size_t, :size_t, :pointer], :size_t
     callback :debug_callback, [:pointer, :debug_info_type, :pointer, :size_t, :pointer], :int
     ffi_lib_flags :now, :global
-    ffi_lib ['libcurl', 'libcurl.so.4']
+
+    # OS X el Capitan's System Integrity Protection (SIP) doesn't allow the use of ENV vars such as DYLD_LIBRARY_PATH,
+    # which previously could be used by dlopen() to find a different version of curl for Ethon to use.
+    #
+    # Therefore, for developers who want to use a non-system version of curl, we need to provide our own such ENV var
+    #
+    # The value should be a full path to the file (not just a dir name)
+    ffi_lib [ENV['ETHON_CURL_LIBFILE'], 'libcurl', 'libcurl.so.4']
   end
 end
